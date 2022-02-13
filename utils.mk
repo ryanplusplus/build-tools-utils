2bcf62117cfdab02bca40daf673f8d4d47cabd12
Ryan Hartlage <ryanplusplus@gmail.com>
2022-02-13 11:36:18 -0500

    Updated utils

--- utils.mk
+++ utils.mk
@@ -1,12 +1,3 @@
-define set_default_flags
-$(eval
-$1_ASFLAGS := $(ASFLAGS)
-$1_CPPFLAGS := $(CPPFLAGS)
-$1_CFLAGS := $(CFLAGS)
-$1_CXXFLAGS := $(CXXFLAGS)
-)
-endef
-
 define create_lib
 $(eval
 LIBS += $1
@@ -14,8 +5,13 @@ LIBS += $1
 endef
 
 define create_lib_with_defaults
-$(call create_lib,$1)
-$(call set_default_flags,$1)
+$(eval
+LIBS += $1
+$1_ASFLAGS := $(ASFLAGS)
+$1_CPPFLAGS := $(CPPFLAGS)
+$1_CFLAGS := $(CFLAGS)
+$1_CXXFLAGS := $(CXXFLAGS)
+)
 endef
 
 define create_interface_lib
@@ -25,8 +21,13 @@ INTERFACE_LIBS += $1
 endef
 
 define create_interface_lib_with_defaults
-$(call create_interface_lib,$1)
-$(call set_default_flags,$1)
+$(eval
+INTERFACE_LIBS += $1
+$1_ASFLAGS := $(ASFLAGS)
+$1_CPPFLAGS := $(CPPFLAGS)
+$1_CFLAGS := $(CFLAGS)
+$1_CXXFLAGS := $(CXXFLAGS)
+)
 endef
 
 define current_path
